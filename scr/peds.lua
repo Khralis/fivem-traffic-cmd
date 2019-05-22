8628a87404952a713b17b030c889d5627c054d00
nickh90 <50895160+nickh90@users.noreply.github.com>
2019-05-22 00:31:16 -0400

    Update peds.lua
    omitted vehicle related code

--- scr/peds.lua
+++ scr/peds.lua
@@ -7,16 +7,10 @@ if pState == "off" then
   Citizen.CreateThread(function(peds)
   while pState == "off"
     do
-      --SetVehicleDensityMultiplierThisFrame(0.0)
       SetPedDensityMultiplierThisFrame(0.0)
-      --SetRandomVehicleDensityMultiplierThisFrame(0.0)
-      --SetParkedVehicleDensityMultiplierThisFrame(0.0)
       SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
       local playerPed = GetPlayerPed(-1)
       local pos = GetEntityCoords(playerPed)
-      --RemoveVehiclesFromGeneratorsInArea(pos['x'] - 500.0, pos['y'] - 500.0, pos['z'] - 500.0, pos['x'] + 500.0, pos['y'] + 500.0, pos['z'] + 500.0);
-      --SetGarbageTrucks(0)
-      --SetRandomBoats(0)
       Citizen.Wait(5)
       if (chatMessage) then break
     end
