66663d285cc2bee4c391cd782f268424b4382844
nickh90 <50895160+nickh90@users.noreply.github.com>
2019-05-21 23:33:51 -0400

    Update traffic.lua

--- scr/traffic.lua
+++ scr/traffic.lua
@@ -8,10 +8,10 @@ if tState == "off" then
   while tState == "off"
     do
       SetVehicleDensityMultiplierThisFrame(0.0)
-      --SetPedDensityMultiplierThisFrame(0.0)
+      SetPedDensityMultiplierThisFrame(0.0)
       SetRandomVehicleDensityMultiplierThisFrame(0.0)
       SetParkedVehicleDensityMultiplierThisFrame(0.0)
-      --SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
+      SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
       local playerPed = GetPlayerPed(-1)
       local pos = GetEntityCoords(playerPed)
       RemoveVehiclesFromGeneratorsInArea(pos['x'] - 500.0, pos['y'] - 500.0, pos['z'] - 500.0, pos['x'] + 500.0, pos['y'] + 500.0, pos['z'] + 500.0);
