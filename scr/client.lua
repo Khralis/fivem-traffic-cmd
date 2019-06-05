ee38a4cd6dcc2d74ce49a0900302ea0cd75f6459
nickh90 <50895160+nickh90@users.noreply.github.com>
2019-06-05 07:50:40 -0400

    Create client.lua

---
+++ scr/client.lua
@@ -0,0 +1,4 @@
+RegisterNetEvent("doNoPerms")
+AddEventHandler("doNoPerms", function()
+TriggerEvent('chat:addMessage', {color = {255, 45, 45},multiline = true, args = {"[Error]: Sorry, but you don't have permission to do this"}})
+end)
