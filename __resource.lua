4365359e5052c35e775189fcb74e89c9339ad65e
nickh90 <50895160+nickh90@users.noreply.github.com>
2019-06-05 07:51:42 -0400

    new permissions scripts

--- __resource.lua
+++ __resource.lua
@@ -1,2 +1,3 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
-client_script {'/scr/traffic.lua', '/scr/peds.lua'}
+client_script {'/scr/traffic.lua', '/scr/peds.lua' ,'/scr/client.lua'}
+server_script {'/scr/server.lua'}
