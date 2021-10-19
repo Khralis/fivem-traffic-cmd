ada9920353adf9a198dbda60dc7fa6b4217059a0
Khralis <50895160+Khralis@users.noreply.github.com>
2021-10-19 19:58:09 -0400

    Update to fxmanifest
    Updated to fxmanifest and added more details.
    fixed clientscript line

---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'adamant'
+game 'gta5'
+name 'fivem-traffic-cmd'
+description 'Toggle FiveM traffic and peds on or off client side. Useful for drift servers and the likes'
+version '1.1'
+client_scripts {'fish ', '/scr/peds.lua', '/scr/client.lua'}
+server_script {'/scr/server.lua'}
