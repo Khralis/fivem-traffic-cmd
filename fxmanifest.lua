ac5e3e105b9eeb7a4e39d2f1908c3c41979ad635
Khralis <50895160+Khralis@users.noreply.github.com>
2023-01-11 09:37:20 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,6 @@ fx_version 'adamant'
 game 'gta5'
 name 'fivem-traffic-cmd'
 description 'Toggle FiveM traffic and peds on or off client side. Useful for drift servers and the likes'
-version '1.4'
+version '1.5'
 client_scripts {'/scr/traffic.lua', '/scr/peds.lua', '/scr/client.lua'}
 server_script {'/scr/server.lua'}
