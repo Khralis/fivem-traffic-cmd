777594537f29d51b81ac1303ba5471e5ffaa420d
Khralis <50895160+Khralis@users.noreply.github.com>
2023-01-11 08:35:08 -0500

    Fixed "Fish"
    Now with 100% less fish.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,6 @@ fx_version 'adamant'
 game 'gta5'
 name 'fivem-traffic-cmd'
 description 'Toggle FiveM traffic and peds on or off client side. Useful for drift servers and the likes'
-version '1.1'
-client_scripts {'fish ', '/scr/peds.lua', '/scr/client.lua'}
+version '1.4'
+client_scripts {'/scr/traffic.lua', '/scr/peds.lua', '/scr/client.lua'}
 server_script {'/scr/server.lua'}
